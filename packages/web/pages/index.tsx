21dabffb91483e9bab8a094a481f3362fa383bd5
h-a-n-a <andywangsy@gmail.com>
2021-07-09 01:41:24 +0800

    feat: init web

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return <div>
+        <h1 className="container px-4 text-5xl">Yoki</h1>
+
+        <div className="flex m-2 px-4">
+            <div className="flex-initial">
+                test
+            </div>
+        </div>
+    </div>
+}
